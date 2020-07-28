28b0a59faf11d35b3c620ad5e7fb141344ae9ef6
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-28 15:37:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,44 +8,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    redirect to '/articles'
+    redirect to "/articles"
   end
-  
-  get '/articles' do 
-    @articles = Articles.all 
-    erb :index 
-  end 
-  
-  get '/articles/new' do 
-    @article = Article.new 
-  end 
-  
-  
-  post '/articles' do 
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+  post "/articles" do
     @article = Article.create(params)
-    redirect to '/articles/#{@article.id}'
-  end 
-  
-  get '/articles/:id' do 
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get "/articles/:id" do
     @article = Article.find(params[:id])
-    erb :show 
-  end 
-  
-  get '/articles/:id/edit' do 
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
     @article = Article.find(params[:id])
-    erb :edit 
-  end 
-  
-  patch '/articles/:id' do 
+    erb :edit
+  end
+
+
+  patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
-    redirect to '/articles/#{@article.id}'
-  end 
-  
-  delete '/articles/:id' do 
+    redirect to "/articles/#{ @article.id }"
+  end
+
+
+  delete "/articles/:id" do
     Article.destroy(params[:id])
-    redirect to '/articles'
-  end 
-  
-  
-end 
+    redirect to "/articles"
+  end
+
+
+end
