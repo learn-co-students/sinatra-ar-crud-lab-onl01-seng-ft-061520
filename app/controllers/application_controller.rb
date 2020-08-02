6ce49774237fb43b17b02bb13e87f0221851cf6b
Ray <DogPa@Rays-MacBook-Pro.local>
2020-08-01 18:29:12 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +8,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get'/articles/new' do
+    erb :new
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  post '/articles' do
+    title = params[:title]
+    content = params[:content]
+    data = {:title => title, :content => content}
+    @article = Article.create(data)
+    url = '/articles/' + @article.id.to_s
+    redirect to(url)
+  end
+
+  patch '/articles/:id' do
+    title = params[:title]
+    content = params[:content]
+    @article = Article.find(params[:id])
+    @article.title = title.split(" ").map do |word|
+      word.capitalize()
+    end
+    @article.title = @article.title.join(" ")
+    @article.content = content
+    @article.save
+    url = '/articles/' + @article.id.to_s
+    redirect to(url)
+
+  end
+
+  delete '/articles/:id' do
+    article = Article.find(params[:id])
+    article.destroy
+    redirect to('/articles')
   end
 end
